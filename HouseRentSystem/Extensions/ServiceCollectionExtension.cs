ed801bc7b3f7d8fb4d29c14edc125abc2d18dd46
Ivan Gerov <ivangerov93@gmail.com>
2024-02-25 17:39:09 +0200

    Add project files.

---
+++ HouseRentSystem/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace HouseRentSystem.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static  IServiceCollection AddApplicationServices(this IServiceCollection service)
+        {
+            return service;
+        }
+    }
+}
---
+++ HouseRentSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HouseRentSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
