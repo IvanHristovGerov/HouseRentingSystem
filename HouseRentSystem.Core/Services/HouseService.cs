32a03b4d2f53df0f0f8a8f77c6e58105e1fcf73b
Ivan Gerov <ivangerov93@gmail.com>
2024-03-15 19:08:53 +0200

    AllAsync method for houses implemented

--- HouseRentSystem.Core/Services/HouseService.cs
+++ HouseRentSystem.Core/Services/HouseService.cs
@@ -1,4 +1,5 @@
 ﻿using HouseRentSystem.Core.Contracts;
+using HouseRentSystem.Core.Enumerations;
 using HouseRentSystem.Core.Models.Home;
 using HouseRentSystem.Core.Models.House;
 using HouseRentSystem.Infrastructure.Data.Common;
@@ -72,6 +73,75 @@ namespace HouseRentSystem.Core.Services
             return house.Id;
         }
 
-        //5
+
+        //5 All Houses methods
+        public async Task<HouseQueryServiceModel> AllAsync(
+            string? category = null,
+            string? searchTerm = null,
+            HouseSorting sorting = HouseSorting.Newest,
+            int currentPage = 1, 
+            int housesPerPage = 1)
+        {
+            var housesToShow = repository.AllReadOnly<House>();
+
+            if (category != null)
+            {
+                housesToShow = housesToShow
+                    .Where(h => h.Category.Name == category);
+            }
+
+            if (searchTerm != null)
+            {
+                string normalizedSearchTerm = searchTerm.ToLower();
+
+                housesToShow = housesToShow
+                    .Where(h => (h.Title.ToLower().Contains(normalizedSearchTerm) ||
+                                 h.Address.ToLower().Contains(normalizedSearchTerm) ||
+                                 h.Description.ToLower().Contains(normalizedSearchTerm)));
+            }
+
+            housesToShow = sorting switch
+            {
+                HouseSorting.Price => housesToShow
+                    .OrderByDescending(h => h.PricePerMonth),
+                HouseSorting.NotRentedFirst => housesToShow
+                    .OrderBy(h => h.RenterId != null)
+                    .ThenByDescending(h => h.Id),
+                _ => housesToShow
+                    .OrderByDescending(h => h.Id)
+
+                //in switch expression _=> means default value
+            };
+
+            //pages
+            var houses = await housesToShow
+                .Skip((currentPage - 1) * housesPerPage)
+                .Take(housesPerPage)
+                .Select(h => new HouseServiceModel()
+                {
+                    Id = h.Id,
+                    Address = h.Address,
+                    ImageUrl = h.ImageUrl,
+                    IsRented = h.RenterId !=null,
+                    PricePerMonth = h.PricePerMonth,
+                    Title = h.Title
+                })
+                .ToListAsync();
+
+            int totalHouses = await housesToShow.CountAsync();
+
+            return new HouseQueryServiceModel()
+            {
+                Houses = houses,
+                TotalHousesCount = totalHouses
+            };
+        }
+
+        public Task<IEnumerable<string>> AllCategoriesNamesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+
     }
 }
