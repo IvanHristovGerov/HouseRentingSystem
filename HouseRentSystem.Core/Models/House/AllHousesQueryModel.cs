3a06a9705d65d4135dd7c78270af7f30746a264e
Ivan Gerov <ivangerov93@gmail.com>
2024-03-15 19:16:59 +0200

    AllHousesQueryModel model implemented

--- HouseRentSystem.Core/Models/House/AllHousesQueryModel.cs
+++ HouseRentSystem.Core/Models/House/AllHousesQueryModel.cs
@@ -1,7 +1,31 @@
-﻿namespace HouseRentSystem.Core.Models.House
+﻿using HouseRentSystem.Core.Enumerations;
+using System.ComponentModel;
+using System.Globalization;
+
+namespace HouseRentSystem.Core.Models.House
 {
     public class AllHousesQueryModel
     {
+        public const int HousesPerPage = 3;
+
+        public string Category { get; set; } = string.Empty;
+
+        [DisplayName("Search by text")]
+        public string SearchTerm { get; set; } = string.Empty;
+
+        public HouseSorting Sorting { get; set; }
+
+        public int CurrentPage { get; set; } = 1;
+
+        public int TotalHousesCount { get; set; }
+
+        public IEnumerable<string> Categories { get; set; } = null!;
+
+        public IEnumerable<HouseServiceModel> Houses { get; set; } = new List<HouseServiceModel>();
+
+
+
+
 
     }
 }
